786134e6d3110040cd149c738e860621f234bf9f
Flors Rueda <ruedaflors@gmail.com>
2025-07-16 13:18:02 +0200

    fix otters picture size

--- src/components/Story.vue
+++ src/components/Story.vue
@@ -8,7 +8,7 @@ const props = defineProps({
 <template>
   <section id="story" class="text-center w-full max-w-11/12 bg-base-100 flex flex-col-reverse md:flex-row justify-center items-center gap-5 min-h-fit h-[500px] py-10 pl-5">
     <div>
-      <img src="/sleepy-otters.svg" class="w-60 h-60 rotate-45 animate-[spin_15s_infinite] self-center"/>
+      <img src="/sleepy-otters.svg" class="w-60 h-60 md:h-96 md:w-96 rotate-45 animate-[spin_15s_infinite] self-center"/>
     </div>
     <div class="w-full md:w-1/2 text-xl">
       <h2 class="text-5xl text-secondary-content font-extrabold font-title pb-2">{{ $props.t.storyTitle }}</h2>
